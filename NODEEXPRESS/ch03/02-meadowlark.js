d3ced8b3ab64166e69868da543bc0b89ead91c1b
HuyDang187 <101924042+HuyDang187@users.noreply.github.com>
2022-11-10 17:24:20 +0700

    10/11/2022

--- NODEEXPRESS/ch03/02-meadowlark.js
+++ NODEEXPRESS/ch03/02-meadowlark.js
@@ -1,22 +1,36 @@
 const express = require('express')
 const expressHandlebars = require('express-handlebars')
-const port = process.env.PORT ||3000
 const app = express()
-app.engine('handlebars',expressHandlebars({
+
+app.engine('handlebars', expressHandlebars.engine({
+    extname: 'handlebars',
     defaultLayout: 'main',
 }))
-app.set('view engine','handlebars')
+app.set('view engine', 'handlebars')
 
-app.get('/', (req,res)=> res.render('home'))
-app.get('/about',(req,res)=>res.render('about'))
+app.use(express.static(__dirname + '/public'))
 
-app.use((req,res)=>{
+const port = process.env.PORT || 3000
+app.get('/', (req, res) => res.render('home'))
+
+
+const fortunes = [
+    "Conquer your fears or they will conquer you.", "Rivers need springs.",
+    "Do not fear what you don't know.",
+    "You will have a pleasant surprise.",
+    "Whenever possible, keep it simple.",
+]
+app.get('/about', (req, res) => {
+    const randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)]
+    res.render('about', { fortune: randomFortune })
+})
+app.use((req, res) => {
     res.status(404)
     res.render('404')
 })
-app.use((err,req,res,next)=>{
+app.use((err, req, res, next) => {
     console.error(err.message)
     res.status(500)
     res.render('500')
 })
-app.listen(port,()=>console.log(`Express started on http://localhost:${port};`+`press Ctrl+C to terminate.`))
+app.listen(port, () => console.log(`Express started on http://localhost:${port};` + `press Ctrl+C to terminate.`))
