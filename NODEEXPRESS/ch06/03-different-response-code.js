76b9acc40e92e60bd6f17a6497f1d47292de4b37
HuyDang187 <101924042+HuyDang187@users.noreply.github.com>
2022-11-15 17:29:17 +0700

    nodeJS
    15/11/2022

---
+++ NODEEXPRESS/ch06/03-different-response-code.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const expressHandlebars = require('express-handlebars')
+const app = express()
+
+app.engine('handlebars', expressHandlebars.engine({ defaultLayout: 'main'}))
+
+app.get('/error',(req,res)=> res.status(500).render('error'))
+
+app.get('*', (req,res)=> res.send('check out our <a href="/error">Error</a> page!'))
+
+const port = process.env.PORT || 3000
+app.listen(port,()=>console.log(`\nnavigate to http://localhost:${port}/error\n`))
