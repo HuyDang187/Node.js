7f87c81708407eed19f96fcc09ceaa8a04a14cc4
HuyDang187 <101924042+HuyDang187@users.noreply.github.com>
2022-11-19 18:18:52 +0700

    exam-nodeJS
    19/11/2022

---
+++ NODEEXPRESS/ch08/lib/middleware/weather.js
@@ -0,0 +1,9 @@
+const getWeatherData = () =>[
+    {
+        location: {
+            name: 'Portland',
+        },
+        forecastUrl: 'http://api.weather.gov/gridpoints/PQR/112,103/forecast',
+        
+    }
+]
